951b219ee97d03a09bbe5839e24b1a058a7a7ce3
sihemTag <t.sihem-26@outlook.com>
2022-02-11 13:00:00 +0100

    mise a jour de testSp

--- testSp.py
+++ testSp.py
@@ -21,14 +21,16 @@ for i in data:
         text = data[i]['title']
         doc = nlp(text)
         # Find named entities, phrases and concepts
+        l=[]
         for entity in doc.ents:
                 if(entity.label_ == 'GPE' or entity.label_ == 'LOC'):
-                        data[i]['SpacyLoc'] = entity.text
-                        data[i]['LabelLoc'] = entity.label_
-
+                        l.append((entity.text,entity.label_))
+                        data[i]['SpacyLoc']=l    
+                                           
 with open('data.json', 'w') as mon_fichier:
     mon_fichier.write(json.dumps(data, indent=4))
 # Closing file
+
 fjson.close()
 
 # RESTE A FAIRE LE FILTRAGE DES IMAGES QUI NE CONTIENT PAS DES LOC OU GPE OU INDICATION QUI AIDE A TROUVER LA LOCALISATION
