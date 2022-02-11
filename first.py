d1da20c4746262aea8a006b8e4f6450367f56323
LydiaOuam <lydiaouamrane@gmail.com>
2022-02-11 11:42:16 +0100

    Update Json File

--- first.py
+++ first.py
@@ -9,7 +9,7 @@ import os
 
 reddit = praw.Reddit(client_id='g6PvbFi9-f_JWC3rCJIO9A', client_secret='7xuZW9s132HfMGX0q5hwX0lF1WQwqA', user_agent='ter')
 
-subreddit = reddit.subreddit("Nature")
+subreddit = reddit.subreddit("EarthPorn")
 count = 0
 dic={}
 
@@ -30,7 +30,7 @@ ignore_path = os.path.join(dir_path, "ignore_images/")
 create_folder(image_path)
 
 # Iterate through top submissions
-for submission in subreddit.top(limit=None):
+for submission in subreddit.hot(limit=50):
 
     # Get the link of the submission
     url = str(submission.url)
@@ -38,14 +38,10 @@ for submission in subreddit.top(limit=None):
 
 # Check if the link is an image
     if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png"):
-        dic[f"image{count}"]=[title,url]
+        dic.update({f"image{count}":{"title":title,"url":url}})
         # Retrieve the image and save it in current folder
         res=urllib.request.urlretrieve(url, f"images/image{count}.png")
         count += 1
-
-        # Stop once you have 10 images
-        if count == 10:
-            break
 
 with open('data.json', 'w') as mon_fichier:
     mon_fichier.write(json.dumps(dic, indent=4))
