02e6fc8fc997852ed638e19dc25fc9258c7323a1
LydiaOuam <lydiaouamrane@gmail.com>
2022-03-25 20:10:02 +0100

    Filtrer le retour de geoname

--- geoname.py
+++ geoname.py
@@ -1,3 +1,4 @@
+from unittest import case
 import geocoder
 
 # Montpellier France
@@ -5,26 +6,26 @@ import geocoder
 # Larslan Church
 
 # Nettoyage de la chaine
-text = "Oregon ".replace(",","").lower() 
-ma_chaine = text.split() 
-print("-------------------------------------------------")
-a = geocoder.geonames("Oregon",key='Lydia_Ouam', maxRows = 5,  isNameRequired = True)
-# print(a.geojson)
-for i in a:
-    g = geocoder.geonames(i.geonames_id, method = 'details', key='Lydia_Ouam')
-    for r in g:
-        if r.admin4 != '':
-            if r.admin4 == r.address:
-                if(r.feature_class == 'P'):
-                    print('----------------Filtrage-----------------') 
-                    # Par la suite on affichera que le premier
-                    print([(r.address, r.country, r.description, r.feature_class, r.class_description, r.admin2, r.admin3, r.admin4, r.lat, r.lng)])
-        # Maintenant on essaie de regarder si y a une chaine specific dans le text example church, airport et la prendre pour afficher
-        
-        if r.country_code.lower() in ma_chaine:
-            print([(r.address, r.country, r.description, r.feature_class, r.class_description, r.admin2, r.admin3, r.admin4, r.lat, r.lng)])
+def geo_loc(text):
+    text = text.replace(",","").lower() 
+    ma_chaine = text.split() 
+
+    a = geocoder.geonames(text,key='Lydia_Ouam', maxRows = 5,  isNameRequired = True)
+
+    for i in a:
+        g = geocoder.geonames(i.geonames_id, method = 'details', key='Lydia_Ouam')
+        for r in g:
+
             if r.description in ma_chaine:
-                print([(r.address, r.country, r.description, r.feature_class, r.class_description, r.admin2, r.admin3, r.admin4, r.lat, r.lng)])
+                localisation = (r.lat, r.lng)
+                if r.country_code.lower() in ma_chaine:
+                    localisation = (r.lat, r.lng)
+                return localisation
 
-
-# country_code
+            if(r.feature_class == 'P'):
+                localisation = (r.lat, r.lng)
+                if r.admin4 != '':
+                    if r.admin4 == r.address:
+                        localisation = (r.lat, r.lng)
+                return localisation
+        return (r.lat,r.lng)
