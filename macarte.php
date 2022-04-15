22399ba2a4eeaf4963812dc01683db28c2893bdc
LydiaOuam <lydiaouamrane@gmail.com>
2022-04-15 12:41:52 +0200

    Style color

--- macarte.php
+++ macarte.php
@@ -26,14 +26,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         
 </head>
-    <form action="macarte.php" method="POST">
-        <button class = "submit" name = "my_button" value="before" id="button" >Click Me</button>
-    </form>
 
+    <!-- <form action="macarte.php" method="POST">
+        <button class = "submit" name = "my_button" value="before" id="button" >Click Me</button>
+    </form> -->
+    <div id="cercle" style = "
+    position: absolute; 
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    background: green;
+    right: 100px;
+    bottom : 900px;
+    z-index:2;
+    "> </div>
 </form>
 <body>    
         <div class="folium-map" id="map_a620533b73fc4a38880428953e8ae81f"></div>
          <div id ="resultat_heuristique"></div> 
+
+
  
 </body>
 <?php 
